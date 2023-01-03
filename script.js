9094e8ea16930b90f20320734b3a4c707989496e
Yuriy <yuriy.bilskiy@gmail.com>
2023-01-03 16:10:06 +0200

    films js practice

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 "use strict";
 
+const number0fFilms = prompt('Сколько фильмов вы просмотрели?', '');
+
+const personalMoviesDB = {
+    count: number0fFilms,
+    movies: {  },
+    actors: {},
+    genres: [],
+    privat: false
+};
+const   a = prompt('Один з остатніх фільмів?', ''),
+            b = prompt('Оцінка фільма?', ''),
+            c = prompt('Один з остатніх фільмів?', ''),
+            d = prompt('Оцінка фільма?', '');
+    personalMoviesDB.movies[a] = b;
+    personalMoviesDB.movies[c] = d;
+
+    console.log(personalMoviesDB);
+
